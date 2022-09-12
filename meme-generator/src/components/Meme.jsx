f0d8f693976a6aa7cbe3a8a7cca4cde86f4c2aef
Austin Williams <senortinman@gmail.com>
2022-09-12 11:09:14 -0400

    Text inputs now render text on image in real time

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -3,8 +3,8 @@ import memesData from "../memesData";
 
 function Meme() {
   const [meme, setMeme] = React.useState({
-    topText: "",
-    bottomText: "",
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
     randomImg: "http://i.imgflip.com/1bij.jpg",
   });
   const [allMemeImg, setAllMemeImg] = React.useState(memesData);
@@ -18,6 +18,18 @@ function Meme() {
       randomImg: url,
     }));
   }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMemeData) => {
+      return {
+        ...prevMemeData,
+        [name]: value,
+      };
+    });
+    console.log(meme);
+  }
+
   return (
     <div className="meme-container">
       <div className="meme-form">
@@ -26,12 +38,18 @@ function Meme() {
           id="top-text"
           placeholder="Top Text"
           className="meme-inputs"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
         />
         <input
           type="text"
           id="bottom-text"
           placeholder="Bottom Text"
           className="meme-inputs"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
         />
         <button className="meme-btn" onClick={getMeme}>
           Generate meme image 🖼️
@@ -39,8 +57,8 @@ function Meme() {
       </div>
       <div className="meme">
         <img src={meme.randomImg} className="meme-img" />
-        <h2 className="meme-text top">One does not simply</h2>
-        <h2 className="meme-text bottom">Walk into Mordor</h2>
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
       </div>
     </div>
   );
